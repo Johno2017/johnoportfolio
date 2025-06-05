0f40b800642640be0a480f43fc4199a6c6989925
John Ong <john.ong135@gmail.com>
2025-06-05 12:37:21 -0700

    Fixed Project card responsiveness

--- src/components/Step.tsx
+++ src/components/Step.tsx
@@ -10,17 +10,17 @@ interface Props {
 function Step({ href, src, name, description }: Props) {
   return (
     <a href={href} target="_blank" className="step">
-      <div className="content">
-          <img alt="Game Screenshot" src={src} className="screenshot" />
-        <div className="info">
-          <h3 className="poppins emerald project-name">{name}</h3>
+      <div className="screenshot">
+        <img alt="Game Screenshot" src={src} className="screenshot" />
+      </div>
+      <div className="info">
+        <h3 className="poppins emerald project-name">{name}</h3>
           <p>{description}</p>
           <div className="go-to-container">
             <div>
               <p className="z-4">Go to &rarr;</p>
             </div>
           </div>
-        </div>
       </div>
     </a>
   );
