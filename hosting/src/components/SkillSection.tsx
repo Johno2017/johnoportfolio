ba13b4b9ad28096ec1920ab430cf9059215a5190
John Ong <john.ong135@gmail.com>
2025-06-06 15:02:50 -0700

    Added in SVG animation

--- hosting/src/components/SkillSection.tsx
+++ hosting/src/components/SkillSection.tsx
@@ -28,6 +28,21 @@ const skillVariants = {
   },
 };
 
+const pathVariants = {
+  hidden: {
+    opacity: 0,
+    pathLength: 0,
+  },
+  visible: {
+    opacity: 1,
+    pathLength: 1,
+    transition: {
+      duration: 2,
+      ease: "easeInOut",
+    },
+  },
+};
+
 let skills = [
   { name: "Node.js" },
   { name: "Html" },
@@ -60,15 +75,58 @@ function SkillSection() {
       whileInView="visible"
       viewport={{ once: true, amount: 0.5 }}
     >
-      <h3>Developer Skills</h3>
+      <div className="skill-title">
+        <motion.svg
+          width="120"
+          height="120"
+          viewBox="0 0 120 120"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <g id="Code">
+            <motion.path
+              variants={pathVariants}
+              id="Icon"
+              d="M68 72L80 60L68 48M52 48L40 60L52 72"
+              stroke="#FFFFFF"
+              stroke-width="5"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+            />
+          </g>
+        </motion.svg>
+        <h3>Developer Skills</h3>
+        <motion.svg
+          width="120"
+          height="120"
+          viewBox="0 0 120 120"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <g id="Code">
+            <motion.path
+              variants={pathVariants}
+              id="Icon"
+              d="M68 72L80 60L68 48M52 48L40 60L52 72"
+              stroke="#FFFFFF"
+              stroke-width="5"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+            />
+          </g>
+        </motion.svg>
+      </div>
+
       <div className="skill-list">
         {skills.map((skill) => (
-          <motion.div className="skill emerald-shadow" 
+          <motion.div
+            className="skill emerald-shadow"
             variants={skillVariants}
             whileHover={{
               scale: 1.1,
-              boxShadow: "5px 5px 5px  #31d097"
-            }}>
+              boxShadow: "5px 5px 5px  #31d097",
+            }}
+          >
             <h6>{skill.name}</h6>
           </motion.div>
         ))}
