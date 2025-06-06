8304905b072cdfc2e5a2da0ddcc686eff31d2afa
John Ong <john.ong135@gmail.com>
2025-06-06 01:18:55 -0700

    Added in Framer Motion with some basic animations

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,41 +1,72 @@
-import '../styles/Main.css';
-import BackgroundInfo from './BackgroundInfo';
-import Projects from './Projects';
-import Footer from './Footer';
-import SkillSection from './SkillSection';
+import "../styles/Main.css";
+import BackgroundInfo from "./BackgroundInfo";
+import Projects from "./Projects";
+import Footer from "./Footer";
+import SkillSection from "./SkillSection";
+import { motion } from "motion/react";
+
+const greetingDivVariants = {
+  hidden: {
+    y: 100,
+    opacity: 0,
+  },
+  visible: {
+    y: 0,
+    opacity: 1,
+    transition: {
+        mass: 0.5,
+        damp: 10,
+        duration: 0.5,
+        when: "beforeChildren",
+        staggerChildren: 0.25
+    }
+  },
+};
 
 function Main() {
   return (
-  <main>
-    <section id="introPage">
-        <div className="title-section">
-            <h2>
-                Hi! I'm <span className="poppins emerald">John</span> Ong
-                <br />Full Stack
-                <span className="poppins emerald"> Developer</span><br></br>
-                <span className="poppins emerald">DevOps</span> Engineer
-            </h2>
-            <p className="description">
-                I specialize as a <span className="emerald">.NET developer</span> with many years of 
-                practical experience working in <span className="emerald">C#, SQL, and JavaScript.</span>
-                I also have experience with CI/CD pipelines and automation in <span className="emerald">Jenkins, GitLab, and Azure DevOps.</span>
-            </p>
-        </div>
-        <div className="head-shot-container blob bg-grey-dark">
-            <div className='blob-inner bg-grey-lite'>
-                <img
-                    alt="Headshot"
-                    src="src\assets\PortfolioPhoto.PNG"
-                    className="head-shot"
-                />
-            </div>
-        </div>
-    </section>
-    <SkillSection/>
-    <Projects />
-    <BackgroundInfo />
-    <Footer/>
-  </main>);
+    <main>
+      <section id="introPage">
+        <motion.div
+          className="title-section"
+          variants={greetingDivVariants}
+          initial="hidden"
+          animate="visible"
+        >
+          <motion.h2 variants={greetingDivVariants}>
+            Hi! I'm <span className="poppins emerald">John</span> Ong
+            <br />
+            Full Stack
+            <span className="poppins emerald"> Developer</span>
+            <br></br>
+            <span className="poppins emerald">DevOps</span> Engineer
+          </motion.h2>
+          <motion.p className="description" variants={greetingDivVariants}>
+            I specialize as a <span className="emerald">.NET developer</span>{" "}
+            with many years of practical experience working in{" "}
+            <span className="emerald">C#, SQL, and JavaScript.</span>I also have
+            experience with CI/CD pipelines and automation in{" "}
+            <span className="emerald">Jenkins, GitLab, and Azure DevOps.</span>
+          </motion.p>
+        </motion.div>
+        <motion.div
+          className="head-shot-container blob bg-grey-dark"
+        >
+          <motion.div className="blob-inner bg-grey-lite">
+            <motion.img
+              alt="Headshot"
+              src="src\assets\PortfolioPhoto.PNG"
+              className="head-shot"
+            />
+          </motion.div>
+        </motion.div>
+      </section>
+      <SkillSection />
+      <Projects />
+      <BackgroundInfo />
+      <Footer />
+    </main>
+  );
 }
 
 export default Main;
--- src/components/SkillSection.tsx
+++ src/components/SkillSection.tsx
@@ -1,4 +1,32 @@
+import { motion } from "motion/react";
 import "../styles/SkillSection.css";
+
+const scrollVariants = {
+  hidden: {
+    opacity: 0
+  },
+  visible : {
+    opacity: 1,
+    transition: {
+      when: "beforeChildren",
+      staggerChildren: 0.1
+    }
+  }
+}
+
+const skillVariants = {
+  hidden: {
+    opacity: 0,
+    y: 50
+  },
+  visible: {
+    opacity: 1,
+    y: 0,
+    transition: {
+      type: 'spring'
+    }
+  }
+}
 
 let skills = [
   { name: "Node.js" },
@@ -6,27 +34,38 @@ let skills = [
   { name: "CSS" },
   { name: "JavaScript" },
   { name: "TypeScript" },
+  { name: "Playwright Unit Testing" },
   { name: "C++" },
   { name: "C#" },
+  { name: "C# Unit Testing" },
   { name: "Python" },
   { name: "React.js" },
   { name: "Svelte.js" },
   { name: "Sql Server" },
   { name: "Jenkins" },
+  { name: "PowerShell" },
+  { name: "Bash" },
+  { name: "Unreal Engine 5" },
+  { name: "Godot" },
 ];
 
 function SkillSection() {
   return (
-    <div className="skill-container">
+    <motion.div className="skill-container"
+    variants={scrollVariants}
+    initial="hidden"
+    whileInView="visible"
+    viewport={{once: true, amount: 0.5}}>
       <h3>Developer Skills</h3>
       <div className="skill-list">
         {skills.map((skill) => (
-          <div className="skill">
+          <motion.div className="skill" 
+          variants={skillVariants}>
             <h6>{skill.name}</h6>
-          </div>
+          </motion.div>
         ))}
       </div>
-    </div>
+    </motion.div>
   );
 }
 
